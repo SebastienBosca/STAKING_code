f8b063413cbbc672f2918a1529045d6172b94069
SebastienBosca <sbosca@wanadoo.fr>
2022-05-06 17:50:28 +0200

    ??

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import Table from 'react-bootstrap/Table';
 import Staking from "./contracts/Staking.json";
 import ReWarD from "./contracts/ReWarD.json";
 import ChainlinkETHprice from "./contracts/ChainlinkETHprice.json";
@@ -27,7 +26,7 @@ class App extends Component {
   
         const instance1 = new web3.eth.Contract(
         Staking.abi,
-        "0x0b3Ea9166a64a8d1B3748d3B4386902cd7415efC", 
+        "0xF230C7E192374C6a5421CD6286862d73472C8E0a", 
       );
         const instance2 = new web3.eth.Contract(
         ChainlinkETHprice.abi,
@@ -53,51 +52,66 @@ class App extends Component {
   
     // récupérer la TVL
     const ETHTVL = await contract1.methods.getETHTVL().call();
-    const TVL = ETHTVL/1000000000000000000;
 
     // récupérer le nombre d'ETH sur le contrat
     const ContractBalance = await contract1.methods.getVaultBalance().call(); 
 
     // récupérer le montant déposé
-    const balance = await contract1.methods.getStakedBalance(accounts[0]).call();
+    const balance = await contract1.methods.getStakedBalance(accounts[0]).call()/1000000000000000000;
  
 
     // récupérer le prix de l'ETH avec Chainlink
     const price = await contract2.methods.getLatestPrice().call();
 
     //récupérer le montant de récompenses acquis 
-    const OldRewards= await contract1.methods.getOldRewards().call();
+    const OldRewards= await contract1.methods.getOldRewards(accounts[0]).call()/1000000000000000000;
+
+    //récupérer la balance en jetons RWD du portefeuille
+    const walletBalance = await contract3.methods.balanceOf(accounts[0]).call()/1000000000000000000;
 
     // Mettre à jour le state 
-   this.setState({ETHTVL:ETHTVL, ContractBalance:ContractBalance, balance:balance, price:price, OldRewards:OldRewards });
+   this.setState({ETHTVL:ETHTVL, ContractBalance:ContractBalance, balance:balance, price:price, OldRewards:OldRewards, walletBalance:walletBalance });
   }; 
 
 
 
   DEPOSIT = async() => {
-    const { accounts, contract1, contract2, contract3, web3 } = this.state;
+    const { accounts, contract1, web3 } = this.state;
     const DamountWei = web3.utils.toWei(this.Damount.value, 'ether');   
   await contract1.methods.deposit().send({from: accounts[0], value: DamountWei});
   this.runInit();
 }
   
   WITHDRAW = async() => {
-    const { accounts, contract1, contract2, web3 } = this.state;
+    const { accounts, contract1, web3 } = this.state;
     const WamountWei = web3.utils.toWei(this.Wamount.value, 'ether');  
     await contract1.methods.withdrawal(WamountWei).send({from: accounts[0]});
     this.runInit();
   }
- 
-  //CLAIM = async() => {
-  //  const { accounts, contract } = this.state;
-  //  const propo = this.propo.value;
-  //  await contract.methods.RegisterProposal(propo).send({from: accounts[0]});
-  //  this.runInit();
-  //}
 
+  UPDATE = async() => {
+    const { accounts, contract1, web3 } = this.state;
+    await contract1.methods.UpdateReward().send({from: accounts[0]});
+    this.runInit();
+  }
+
+  CLAIM = async() => {
+    const { accounts, contract1, web3 } = this.state;
+    await contract1.methods.ClaimRewards().send({from: accounts[0]});
+    this.runInit();
+  }
+
+
+  WACR = async() => {
+    const { accounts, contract1, web3 } = this.state;
+    await contract1.methods.WithdrawaAllAndClaimRewards().send({from: accounts[0]});
+    this.runInit();
+  }
+
+ 
 
   render() {
-    const { ETHTVL, ContractBalance, balance, price, OldRewards} = this.state;
+    const { ETHTVL, ContractBalance, balance, price, OldRewards, walletBalance} = this.state;
    
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
@@ -105,36 +119,41 @@ class App extends Component {
 
     return (
       <div className="App">
+
         <div>
             <h2 className="text-center">ETH Vault</h2>
             <hr></hr>
             <br></br>
         </div>
+
         <div style={{display: 'flex', justifyContent: 'center'}}>
           <Card style={{ width: '50rem' }}>
             <Card.Header><strong> ETH price : {price} $ </strong></Card.Header>
             <Card.Header><strong>TVL : {ETHTVL/1000000000000000000} ETH </strong></Card.Header>
-            <Card.Header><strong>Contract Balance: {ContractBalance/1000000000000000000} </strong></Card.Header>
-            <Card.Header><strong>Your staked balance: {balance}  </strong></Card.Header>
-            <Card.Header><strong>Your rewards stored on-chain: {balance}  </strong></Card.Header>
-            
+            <Card.Header><strong>Contract Balance: {ContractBalance/1000000000000000000} ETH </strong></Card.Header>
+            <Card.Header><strong>Your staked balance: {balance} ETH </strong></Card.Header>
+            <Card.Header><strong>Your rewards stored on-chain: {OldRewards} RWD </strong></Card.Header>
+            <Card.Header><strong>In your wallet: {walletBalance} RWD </strong></Card.Header>     
           </Card>
         </div>
+
         <br></br>
+
         <div style={{display: 'flex', justifyContent: 'center'}}>
-          <Card style={{ width: '50rem' }}>
-            <Card.Header><strong> I want to deposit: </strong></Card.Header>
-            <Card.Body>   
+           <Card style={{ width: '50rem' }}>
+             <Card.Header><strong> I want to deposit: </strong></Card.Header>
+             <Card.Body>   
                 <Form.Group controlId="formAddress">
                 <Form.Control type="text" id="Damount"
                 ref={(input) => { this.Damount = input }}
                 />
                 </Form.Group>    
-              <Button onClick={ this.DEPOSIT } variant="dark" > DEPOSIT </Button>
-            </Card.Body>
-          </Card>
-          </div>
-          <div style={{display: 'flex', justifyContent: 'center'}}>
+                <Button onClick={ this.DEPOSIT } variant="dark" > DEPOSIT </Button>
+             </Card.Body>
+           </Card>
+        </div>
+
+        <div style={{display: 'flex', justifyContent: 'center'}}>
           <Card style={{ width: '50rem' }}>
             <Card.Header><strong> I want to withdraw: </strong></Card.Header>
             <Card.Body>
@@ -143,11 +162,44 @@ class App extends Component {
                 ref={(input) => { this.Wamount = input }}
                 />
                 </Form.Group>
-            <Button onClick={ this.WITHDRAW } variant="dark" > WITHDRAW </Button>     
+                <Button onClick={ this.WITHDRAW } variant="dark" > WITHDRAW </Button>     
             </Card.Body>
           </Card>
-          </div>
+        </div>
+
         <br></br>
+    
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Card style={{ width: '50rem' }}>
+            <Card.Header><strong> Recalculing your rewards until now: </strong></Card.Header>
+            <Card.Body>
+                <Button onClick={ this.UPDATE } variant="dark" > Update Rewards </Button>     
+            </Card.Body>
+          </Card>
+        </div>
+
+        <br></br>
+
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Card style={{ width: '50rem' }}>
+            <Card.Header><strong> Sending rewards to your wallet: </strong></Card.Header>
+            <Card.Body>
+                <Button onClick={ this.CLAIM } variant="dark" > Claim Rewards </Button>     
+            </Card.Body>
+          </Card>
+        </div>
+       
+        <br></br>
+
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <Card style={{ width: '50rem' }}>
+            <Card.Header><strong> Sending staked ETH + rewards to your wallet: </strong></Card.Header>
+            <Card.Body>
+                <Button onClick={ this.WACR } variant="dark" > Withdraw All and Claim Rewards </Button>     
+            </Card.Body>
+          </Card>
+        </div>
+
       </div>
     );
 
