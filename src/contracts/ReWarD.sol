4f3062d03c9a54e4ef26e1363d1dfd1aa7896f2c
SebastienBosca <38952196+SebastienBosca@users.noreply.github.com>
2022-06-04 16:19:51 +0200

    Update ReWarD.sol

--- src/contracts/ReWarD.sol
+++ src/contracts/ReWarD.sol
@@ -3,14 +3,19 @@ pragma solidity 0.8.13;
  
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
 import '@openzeppelin/contracts/access/Ownable.sol';
- 
+
 contract ReWarD is ERC20, Ownable {
 	constructor() ERC20('ReWarD', 'RWD') {} 
 
-	// fonction Mint pour créer des tokens
+  Staking staking;
+
+  function SetApprovedStakingAddress (Staking StakingAddress) public {
+     require (msg.sender==owner(), "not owner");
+     staking = StakingAddress;
+  } 
+
 	function Mint(address recipient, uint amount) external {
-        require (msg.sender == owner(), "not owner"); 
+        require (msg.sender == owner() || msg.sender == address(staking), "not owner nor staking contract"); 
 		_mint(recipient, amount);
 	}
 }
-
